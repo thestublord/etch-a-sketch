192f89d1133a11428fcc610595ae7c5a4bab2a7a
Nicholas VanBuskirk <thestublord@gmail.com>
2023-09-04 04:31:23 -0700

    added darkening affect to js file

--- eas.js
+++ eas.js
@@ -10,7 +10,21 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
+        square.style.backgroundColor = 'rgb(255, 255, 255)';
+        square.dataset.interactions = '0';
+        square.addEventListener('mouseover', changeColor);
         container.appendChild(square);
+    }
+}
+
+function changeColor() {
+    let interactions = parseInt(this.dataset.interactions);
+    if (interactions < 10) {
+        let currentColor = this.style.backgroundColor;
+        let colors = currentColor.match(/\d+/g);
+        let newColors = colors.map(color => Math.max(color - 25, 0));
+        this.style.backgroundColor = `rgb(${newColors[0]}, ${newColors[1]}, ${newColors[2]})`;
+        this.dataset.interactions = interactions + 1;
     }
 }
 
